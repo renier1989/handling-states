c57f6d8cdb6ed432ee62019765645c65cfb8a041
Renier Vargas Homestead <renier1989@gmail.com>
2023-06-23 22:28:12 +0000

    añadiendo mas complejidad y pasos a la practica con programacion imperativa. clase 10

--- src/UseState/index.jsx
+++ src/UseState/index.jsx
@@ -3,11 +3,14 @@ const SECURITY_CODE = 'colossus_1989';
 function UseState({name}) {
 
     // AQUI CREAMOS UN ESTADO COMPUESTO Y ASI PODER CONTROLAR VARIOS ESTADOS , IMPORTANTE , AHORA TODOS LOS ESTADOS TIENE QUE EMPEZAR CON EL NOMBRE QUE LE ASIGNAMOS AL ESTADO COMPOUESTO POR EJEMPLO state.value .....
+    // OTRA TEMA IMPORTANTE A TENER EN CUENTA ES QUE AHORA PARA LOS MODIFICADORES SE DEBE PASA TODO EL ESTADO Y LUEGO LO QUE REALMENTE QUEREMOS MODIFICAR, ESTO PARA EVITAR OMITIR ESTADOS EN LOS MODIFICADORES, POR EJEMPLOS setState(...state, estodo: modificacion)
 
     const [state, setState] = React.useState({
         value : '',
         error: false,
-        loading : false
+        loading : false,
+        deleted : false, // estado para pasar a la vista de eliminacion y pregunta por confirmacion
+        confirmed : false, // esato para pasar a la visra de eliminacion correcta
     });
 
     console.log(state);
@@ -26,16 +29,18 @@ function UseState({name}) {
         // comprobamos cuando el estado de loading sea realmente true para ejecutar la validacion 
         if(state.loading){
             // vamos a simular una carga de 3 segundos
-            // setError(false);
+            
             setTimeout(() => {
                 console.log('Empezando la validacion');
                 if(state.value === SECURITY_CODE){
                     setState({
                         ...state,
                         error:false,
-                        loading:false
+                        loading:false,
+                        deleted: true,
+                        confirm:false
                     });
-                    // setError(true);
+                    
                 }else{
                     setState({
                         ...state,
@@ -53,37 +58,79 @@ function UseState({name}) {
     }, [state.loading]);
 
 
-    return (
-        <div className='flex flex-col gap-2 bg-slate-200 py-4 px-4 rounded-sm outline outline-2 outline-gray-400'>
-            <h2 className='text-2xl'>Eliminar {name}</h2>
-            <p className='text-lg font-semibold'>Por favor, escribe el código de seguridad</p>
-            {/* AQUI VALID SI HAY UN ERROR Y EL ESTADO DE CARGO ES FALSO */}
-            {(state.error && !state.loading) && (
-                <p className="flex items-center justify-center text-red-500 font-medium">Error: El código es incorrecto</p>
-            )}
-            {state.loading && (
-                <p className="flex items-center justify-center text-indigo-500 font-medium">Comprobando código de seguridad. </p>
-            )}
-
-            <div className='flex flex-row gap-4'>
-                <input value={state.value} onChange={((event)=>{
-                    setState({
+    if(!state.deleted && !state.confirmed){ // qui mostramos la primera parte , donde la persona debe ingresar el codigo y verificar si esta bien escrito
+        return (
+            <div className='flex flex-col gap-2 bg-slate-200 py-4 px-4 rounded-sm outline outline-2 outline-gray-400'>
+                <h2 className='text-2xl'>Eliminar {name}</h2>
+                <p className='text-lg font-semibold'>Por favor, escribe el código de seguridad</p>
+                {/* AQUI VALID SI HAY UN ERROR Y EL ESTADO DE CARGO ES FALSO */}
+                {(state.error && !state.loading) && (
+                    <p className="flex items-center justify-center text-red-500 font-medium">Error: El código es incorrecto</p>
+                )}
+                {state.loading && (
+                    <p className="flex items-center justify-center text-indigo-500 font-medium">Comprobando código de seguridad. </p>
+                )}
+    
+                <div className='flex flex-row gap-4 '>
+                    <input value={state.value} onChange={((event)=>{
+                        setState({
+                            ...state,
+                            value: event.target.value
+                        })
+                        // setValue(event.target.value);
+                    })} className="outline outline-2 outline-gray-400 p-1 rounded-sm" placeholder='Código de seguridad' />
+                    {/* <button onClick={()=>setError(!error)} className='p-2 rounded-sm outline outline-2 outline-green-600 hover:bg-green-700 hover:bg-opacity-70 hover:text-white font-semibold hover:outline-green-900 transition duration-200'>Comprobar</button> */}
+                    <button 
+                    onClick={()=>setState({
                         ...state,
-                        value: event.target.value
-                    })
-                    // setValue(event.target.value);
-                })} className="outline outline-2 outline-gray-400 p-1 rounded-sm" placeholder='Código de seguridad' />
-                {/* <button onClick={()=>setError(!error)} className='p-2 rounded-sm outline outline-2 outline-green-600 hover:bg-green-700 hover:bg-opacity-70 hover:text-white font-semibold hover:outline-green-900 transition duration-200'>Comprobar</button> */}
-                <button 
-                onClick={()=>setState({
-                    ...state,
-                    loading: !state.loading
-                })} 
-                // onClick={()=>setLoading(!state.loading)} 
-                className='p-2 rounded-sm outline outline-2 outline-green-600 hover:bg-green-700 hover:bg-opacity-70 hover:text-white font-semibold hover:outline-green-900 transition duration-200'>Comprobar</button>
+                        loading: !state.loading
+                    })} 
+                    // onClick={()=>setLoading(!state.loading)} 
+                    className='p-2 rounded-sm outline outline-2 outline-green-600 hover:bg-green-700 hover:bg-opacity-70 hover:text-white font-semibold hover:outline-green-900 transition duration-200'>Comprobar</button>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }else if(state.deleted && !state.confirmed){ // aqui muestro la vista para confirmar la eliminacion , por que el codigo que escribio fue correcto
+        return(
+            <div className='flex flex-col gap-2 bg-slate-200 py-4 px-4 rounded-sm outline outline-2 outline-gray-400'>
+                <h2 className='text-2xl'>Eliminar {name}</h2>
+                <p className='text-lg font-semibold'>Por favor, Confirme si realmente desea elminar este estado. ¿Segur@?</p>
+                <div className='flex flex-row gap-4 items-center justify-center'>
+                    <button 
+                    onClick={()=>setState({
+                        ...state,
+                        confirmed:true
+                    })} 
+                    className='p-2 rounded-sm outline outline-2 outline-green-600 hover:bg-green-700 hover:bg-opacity-70 hover:text-white font-semibold hover:outline-green-900 transition duration-200'>Si, Eliminar</button>
+                    <button 
+                    onClick={()=>setState({
+                        ...state,
+                        deleted: false,
+                        value: ''
+                    })} 
+                    className='p-2 rounded-sm outline outline-2 outline-pink-600 hover:bg-pink-700 hover:bg-opacity-70 hover:text-white font-semibold hover:outline-red-900 transition duration-200'>No, Volver</button>
+                </div>
+            </div>
+        );
+    }else { // aqui muestro la vista final de que se elimino con exito
+        return(
+            <div className='flex flex-col gap-2 bg-slate-200 py-4 px-4 rounded-sm outline outline-2 outline-gray-400'>
+                {/* <h2 className='text-2xl'>Eliminar {name}</h2> */}
+                <p className='text-2xl font-semibold'>Gracias por usar esta Aplicación</p>
+                <div className='flex flex-row gap-4 items-center justify-center'>
+                    <button 
+                    onClick={()=>setState({
+                        ...state,
+                        confirmed:false,
+                        deleted: false,
+                        value: ''
+                    })} 
+                    className='p-2 rounded-sm outline outline-2 outline-green-600 hover:bg-green-700 hover:bg-opacity-70 hover:text-white font-semibold hover:outline-green-900 transition duration-200'>Iniciar todo</button>
+                </div>
+            </div>
+        );
+    }
+
 }
 
 export {UseState}
